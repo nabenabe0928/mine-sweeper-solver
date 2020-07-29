e86417bec21e84acb1bb3fbe595dc5f4796f7691
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2020-07-29 16:16:04 +0900

    refactored py files

--- MineSweeper.py
+++ MineSweeper.py
@@ -2,155 +2,142 @@ import numpy as np
 import random
 import math
 
+
 class MineSweeper():
-  def __init__(self, difficulty):
-    self.width = [9, 16, 30][difficulty]
-    self.height = [9, 16, 16][difficulty]
-    self.B = [10, 40, 100][difficulty]
-    self.field = [[0 for w in range(self.width)] for h in range(self.height)]
-    self.cell = [[-1 for w in range(self.width)] for h in range(self.height)]
-    self.zero = [[False for w in range(self.width)] for h in range(self.height)]
-    self.over = False
-    self.clear = False
+    def __init__(self, difficulty):
+        self.width = [9, 16, 30][difficulty]
+        self.height = [9, 16, 16][difficulty]
+        self.B = [10, 40, 100][difficulty]
+        self.field = [[0 for w in range(self.width)] for h in range(self.height)]
+        self.cell = [[-1 for w in range(self.width)] for h in range(self.height)]
+        self.zero = [[False for w in range(self.width)] for h in range(self.height)]
+        self.over = False
+        self.clear = False
 
-  def start(self, x, y):
-	
-    # when opening first panel, you have to call start and specify which position you would like to open.
-    ard = self.around(x, y)
-	
-    # first, get the array filled with rondom number whose size is same as the field itself.
-    place_bomb = np.array([random.random() for i in range(self.width * self.height)])
- 
-    # order the random number in the array and just get the arguments of this array.
-    order = np.argsort(place_bomb)
-    bomb_place = []
+    def start(self, x, y):
+        # when opening first panel, you have to call start and specify which position you would like to open.
+        ard = self.around(x, y)
+        # first, get the array filled with rondom number whose size is same as the field itself.
+        place_bomb = np.array([random.random() for i in range(self.width * self.height)])
+        # order the random number in the array and just get the arguments of this array.
+        order = np.argsort(place_bomb)
+        bomb_place = []
 
-    b = 0
-    t = 0
-    while b < self.B:
-      # get the (t + 1)-th minimum random number in the place_bomb array
-      # if the position of order[t] is corresponds to the panel where you firstly choose at start,
-      # then you will remove the t from the bomb candidates and see next candidates.
-      bomb_candidate = self.num_to_position(order[t])
+        b = 0
+        t = 0
+        while b < self.B:
+            """
+            get the (t + 1)-th minimum random number in the place_bomb array
+            if the position of order[t] is corresponds to the panel where you firstly choose at start,
+            then you will remove the t from the bomb candidates and see next candidates.
+            """
+            bomb_candidate = self.num_to_position(order[t])
 
-      if not bomb_candidate in ard and bomb_candidate != [x, y]:
-        self.field[bomb_candidate[1]][bomb_candidate[0]] = -2
-        b += 1
-        bomb_place.append(bomb_candidate)
-	
-      t += 1
+            if bomb_candidate not in ard and bomb_candidate != [x, y]:
+                self.field[bomb_candidate[1]][bomb_candidate[0]] = -2
+                b += 1
+                bomb_place.append(bomb_candidate)
 
-    for h in range(self.height):
-      for w in range(self.width):
-        position = [w, h]
-	
-        if position in bomb_place:
-          continue
-	
-        p = self.around(w, h)
+            t += 1
+
+        for h in range(self.height):
+            for w in range(self.width):
+                position = [w, h]
+                if position in bomb_place:
+                    continue
+                p = self.around(w, h)
+                count = 0
+
+                for pi in p:
+                    if self.field[pi[1]][pi[0]] == -2:
+                        count += 1
+                self.field[h][w] = count
+        self.open(x, y)
+
+    def count(self, array, value):
         count = 0
-	
-        for pi in p:
-          if self.field[pi[1]][pi[0]] == -2:
-            count += 1
-	
-        self.field[h][w] = count
+        for h in range(self.height):
+            for w in range(self.width):
+                if array[h][w] == value:
+                    count += 1
+        return count
 
-    self.open(x, y)
+    def num_to_position(self, num):
+        h = math.floor(num / self.width)
+        w = num - (self.width * h)
+        return [w, h]
 
-  def count(self, array, value):
-    count = 0
-    for h in range(self.height):
-      for w in range(self.width):
-        if array[h][w] == value:
-          count += 1
-	
-    return count
-
-  def num_to_position(self, num):
-    h = math.floor(num / self.width)
-    w = num - (self.width * h) 
-    return [w, h]
-
-  def out_of_field(self, x, y):
-    if x < 0 or x >= self.width or y < 0 or y >= self.height:
-      return True
-    else:
-      return False
-
-  def around(self, x, y):
-    ard = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-    p = [[x + a[0], y + a[1]] for a in ard]
-	
-    for i in range(len(p) - 1, -1, -1):
-      if self.out_of_field(p[i][0], p[i][1]):
-        del p[i]
-	
-    return p
-
-  def open(self, x, y):
-    self.cell[y][x] = self.field[y][x]
-    if self.cell[y][x] == -2:
-      self.over = True
-
-    while self.count(self.zero,True) != self.count(self.cell,0):
-      self.open_around_zero()
-
-    count = 0
-
-    for h in range(self.height):
-      for w in range(self.width):
-        if self.cell[h][w] == -1:
-          count += 1
-
-    if count == self.B and not self.over:
-      self.clear = True
-
-    self.PlotField()
-
-  def SearchAround(self, x, y):
-    around = self.around(x, y)
-
-    for p in around:
-      if self.field[p[1]][p[0]] == 0:
-        self.cell[p[1]][p[0]] = 0
-	
-  def open_around_zero(self):
-    for h in range(self.height):
-      for w in range(self.width):
-        if self.cell[h][w] == 0:
-          self.zero[h][w] = True
-	
-          around = self.around(w, h)
-          for a in around:
-            self.cell[a[1]][a[0]] = self.field[a[1]][a[0]]
-
-  def GetCellInfo(self, x, y):
-    return self.cell[y][x]
-
-  def PlotField(self):
-    self.print_judge()
-
-    for h in range(self.height):
-      for w in range(self.width):			
-        if self.cell[h][w] == -1:
-          print("x ", end = "")
-        elif self.field[h][w] >= 0 and self.cell[h][w] >= 0:
-          print("{} ".format(self.cell[h][w]), end = "")
+    def out_of_field(self, x, y):
+        if x < 0 or x >= self.width or y < 0 or y >= self.height:
+            return True
         else:
-          print("@ ", end = "")
-				
-      print("")
-    print("")
+            return False
 
-  def print_judge(self):
-    if self.over:
-      print("*******************")
-      print("***  game over  ***")
-      print("*******************")
-      print("")
-    elif self.clear:
-      print("*******************")
-      print("*** game clear! ***")
-      print("*******************")
-      print("")
+    def around(self, x, y):
+        ard = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+        p = [[x + a[0], y + a[1]] for a in ard]
+
+        for i in range(len(p) - 1, -1, -1):
+            if self.out_of_field(p[i][0], p[i][1]):
+                del p[i]
+        return p
+
+    def open(self, x, y):
+        self.cell[y][x] = self.field[y][x]
+        if self.cell[y][x] == -2:
+            self.over = True
+        while self.count(self.zero, True) != self.count(self.cell, 0):
+            self.open_around_zero()
+        count = 0
+
+        for h in range(self.height):
+            for w in range(self.width):
+                if self.cell[h][w] == -1:
+                    count += 1
+
+        if count == self.B and not self.over:
+            self.clear = True
+        self.PlotField()
+
+    def SearchAround(self, x, y):
+        around = self.around(x, y)
+        for p in around:
+            if self.field[p[1]][p[0]] == 0:
+                self.cell[p[1]][p[0]] = 0
+
+    def open_around_zero(self):
+        for h in range(self.height):
+            for w in range(self.width):
+                if self.cell[h][w] == 0:
+                    self.zero[h][w] = True
+                    around = self.around(w, h)
+                    for a in around:
+                        self.cell[a[1]][a[0]] = self.field[a[1]][a[0]]
+
+    def GetCellInfo(self, x, y):
+        return self.cell[y][x]
+
+    def PlotField(self):
+        self.print_judge()
+        for h in range(self.height):
+            for w in range(self.width):
+                if self.cell[h][w] == -1:
+                    print("x ", end="")
+                elif self.field[h][w] >= 0 and self.cell[h][w] >= 0:
+                    print("{} ".format(self.cell[h][w]), end="")
+                else:
+                    print("@ ", end="")
+            print("")
+        print("")
+
+    def print_judge(self):
+        if self.over:
+            print("*******************")
+            print("***  game over  ***")
+            print("*******************")
+            print("")
+        elif self.clear:
+            print("*******************")
+            print("*** game clear! ***")
+            print("*******************")
+            print("")
--- player.py
+++ player.py
@@ -3,131 +3,124 @@ from probability import probability
 import math
 import numpy as np
 
+
 class _game():
-  def __init__(self, player):
-    self.player = player
-    self.W = player.width
-    self.H = player.height
-    self.flag = [[False for h in range(self.H)] for w in range(self.W)]
-    self.n_open = self.W * self.H
+    def __init__(self, player):
+        self.player = player
+        self.W = player.width
+        self.H = player.height
+        self.flag = [[False for h in range(self.H)] for w in range(self.W)]
+        self.n_open = self.W * self.H
 
-   def GetFlag(self):
-    for w in range(self.W):
-      for h in range(self.H):
-        if self.player.GetCellInfo(w, h) != -1:
-          count = 0
-          around = self.player.around(w, h)
-		
-          for a in around:
-            if self.player.GetCellInfo(a[0], a[1]) == -1:
-              count += 1
-					
-          if count == self.player.GetCellInfo(w, h):
-            for a in around:
-              if self.player.GetCellInfo(a[0], a[1]) == -1:
-                self.flag[a[0]][a[1]] = True
+    def GetFlag(self):
+        for w in range(self.W):
+            for h in range(self.H):
+                if self.player.GetCellInfo(w, h) != -1:
+                    count = 0
+                    around = self.player.around(w, h)
+                    for a in around:
+                        if self.player.GetCellInfo(a[0], a[1]) == -1:
+                            count += 1
 
-  def OpenSafe(self):
-    for w in range(self.W):
-      for h in range(self.H):
-        if self.player.GetCellInfo(w, h) != -1:
-          count = 0
-          around = self.player.around(w, h)
-		
-          for a in around:
-            if self.flag[a[0]][a[1]]:
-              count += 1
-	
-          if count == self.player.GetCellInfo(w, h):
-            for a in around:
-              if not self.flag[a[0]][a[1]] and self.player.GetCellInfo(a[0], a[1]) == -1:
-                self.player.open(a[0], a[1])
+                    if count == self.player.GetCellInfo(w, h):
+                        for a in around:
+                            if self.player.GetCellInfo(a[0], a[1]) == -1:
+                                self.flag[a[0]][a[1]] = True
 
-  def cannot_open(self):
-    n_open = 0
+    def OpenSafe(self):
+        for w in range(self.W):
+            for h in range(self.H):
+                if self.player.GetCellInfo(w, h) != -1:
+                    count = 0
+                    around = self.player.around(w, h)
+                    for a in around:
+                        if self.flag[a[0]][a[1]]:
+                            count += 1
 
-    for w in range(self.W):
-      for h in range(self.H):
-        if self.player.GetCellInfo(w, h) >= 0:
-          n_open += 1
+                    if count == self.player.GetCellInfo(w, h):
+                        for a in around:
+                            if not self.flag[a[0]][a[1]] and self.player.GetCellInfo(a[0], a[1]) == -1:
+                                self.player.open(a[0], a[1])
 
-    if n_open == self.n_open:
-      return True
-    else:
-      self.n_open = n_open
-      return False
+    def cannot_open(self):
+        n_open = 0
+        for w in range(self.W):
+            for h in range(self.H):
+                if self.player.GetCellInfo(w, h) >= 0:
+                    n_open += 1
 
-  def num_to_position(self, num):
-    h = math.floor(num / self.W)
-    w = num - (self.W * h) 
-    return [w, h]
+        if n_open == self.n_open:
+            return True
+        else:
+            self.n_open = n_open
+            return False
 
-  def open_land(self):
-    for w in range(self.W):
-      for h in range(self.H):
-        if self.player.GetCellInfo(w, h) == -1:
-          land = True
-	
-          around = self.player.around(w, h)
-          for a in around:
-            if self.player.GetCellInfo(a[0], a[1]) != -1:
-              land = False
-	
-          if land:
-            self.player.open(w, h)
-            return None
+    def num_to_position(self, num):
+        h = math.floor(num / self.W)
+        w = num - (self.W * h)
+        return [w, h]
 
-		
+    def open_land(self):
+        for w in range(self.W):
+            for h in range(self.H):
+                if self.player.GetCellInfo(w, h) == -1:
+                    land = True
+                    around = self.player.around(w, h)
+                    for a in around:
+                        if self.player.GetCellInfo(a[0], a[1]) != -1:
+                            land = False
+                    if land:
+                        self.player.open(w, h)
+                        return None
+
 
 def main(player):
-  game =_game(player) 
-  game.player.start(int(game.W / 2), int(game.H / 2))
-  game.GetFlag()
-  game.OpenSafe()
-
-  while not game.player.over and not game.player.clear:
+    game = _game(player)
+    game.player.start(int(game.W / 2), int(game.H / 2))
     game.GetFlag()
     game.OpenSafe()
 
-    if game.cannot_open():
-      prob = probability(game.player, game.flag)
-      target, p_land = prob.searching()
-      print(target, p_land)
+    while not game.player.over and not game.player.clear:
+        game.GetFlag()
+        game.OpenSafe()
 
-      if 0 in target[2]:
-        idx = []
-        for i, tar in enumerate(target[2]):
-          if tar == 0:
-            position = game.num_to_position(target[0][i])
-            game.player.open(position[0], position[1])
-          elif tar == 1:
-            position = game.num_to_position(target[0][i])
-            game.flag[position[0]][position[1]] = True
-		
-      elif len(target[2]) != 0 and min(target[2]) <  p_land:
-        position = game.num_to_position(target[0][np.argmin(target[2])])
-        game.player.open(position[0], position[1])
-	
-      else:
-        game.open_land()
-	
-	return game.player.clear		
+        if game.cannot_open():
+            prob = probability(game.player, game.flag)
+            target, p_land = prob.searching()
+            print(target, p_land)
 
-### easy   950/1000
-### normal 823/1000
-### hard    42/100
+            if 0 in target[2]:
+                for i, tar in enumerate(target[2]):
+                    if tar == 0:
+                        position = game.num_to_position(target[0][i])
+                        game.player.open(position[0], position[1])
+                    elif tar == 1:
+                        position = game.num_to_position(target[0][i])
+                        game.flag[position[0]][position[1]] = True
+
+            elif len(target[2]) != 0 and min(target[2]) < p_land:
+                position = game.num_to_position(target[0][np.argmin(target[2])])
+                game.player.open(position[0], position[1])
+            else:
+                game.open_land()
+
+    return game.player.clear
+
 
 if __name__ == "__main__":
-	n_win = 0
-	n_game = 1
-	difficulty = 2
+    """
+    easy   950/1000
+    normal 823/1000
+    hard    42/100
+    """
+    n_win = 0
+    n_game = 100
+    difficulty = 0
 
-	for n in range(n_game):
+    for n in range(n_game):
+        player = MineSweeper(difficulty)
+        n_win += main(player)
+        print("{}: winning {}".format(n + 1, n_win))
+        print("")
 
-		player = MineSweeper(difficulty)
-		n_win += main(player)
-		print("{}: winning {}".format(n + 1, n_win))
-		print("")
-
-	print("winning rate: {:.3f} %".format(100 * float(n_win) / float(n_game)))
-	
+    print("winning rate: {:.3f} %".format(100 * float(n_win) / float(n_game)))
